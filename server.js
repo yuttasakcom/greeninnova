78c75f2eb2b153b0787e117adf03ddec8d5c518c
YoProgrammer <yuttasakcom@gmail.com>
2017-11-17 22:19:39 +0700

    create static index.html

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
