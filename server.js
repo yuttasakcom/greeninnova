95e721c5e162200cf4d91d338a6ddff605f5652c
YoProgrammer <yuttasakcom@gmail.com>
2017-11-18 00:36:35 +0700

    updated static page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.resolve(__dirname, 'src')))
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
